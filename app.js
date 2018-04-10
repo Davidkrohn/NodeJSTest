d514b4f35edc9f31e4e04267f1ea4e62251827c8
Davidkrohn <36970676+Davidkrohn@users.noreply.github.com>
2018-04-10 16:09:45 +0200

    update 9

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
+var test = require('unit.js');
+var str = 'Hello, world!';
 
-app.get('/', function (req, res) {
-    res.render('index');
-})
+test.string(str).startsWith('Hello');
+
+if (test.string(str).startsWith('Hello')) {
+    console.log('Passed');
+}
